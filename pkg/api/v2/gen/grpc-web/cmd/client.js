5f5048b6d3b26428620b0b0aa1dc8ce7881dbaaf
namnnp <namnnp@vng.com.vn>
2020-10-05 11:06:03 +0700

    grpc-web: server stream

--- pkg/api/v2/gen/grpc-web/cmd/client.js
+++ pkg/api/v2/gen/grpc-web/cmd/client.js
@@ -1,14 +1,17 @@
-const { MessagePing, MessagePong } = require('../gen/common_pb.js');
+const { MessagePing, MessagePong, StreamingMessagePing, StreamingMessagePong } = require('../gen/common_pb.js');
 
 const { ServiceAClient: ServiceAClient } = require('../gen/rest-service_grpc_web_pb.js');
 const { ServiceAClient: ServiceAClientBinary } = require('../gen/rest-service_grpc_web_pb_binary.js');
 
+const { ServiceExtraClient } = require('../gen/rest-service-extra_grpc_web_pb');
+const { ServiceExtraClient: ServiceExtraClientBinary } = require('../gen/rest-service-extra_grpc_web_pb_binary');
 
 // set xhr2 to nodeJs can call gRPC-Web
 global.XMLHttpRequest = require('xhr2');
 
 
-// const { ServiceExtraClient } = require('../rest-service-extra_grpc_web_pb.js');
+// interceptor implementation
+const StreamResponseInterceptor = function () { }
 
 const TARGET = {
     ENVOY: "http://localhost:7070"
@@ -19,6 +22,8 @@ console.log("envoy_host =>", TARGET.ENVOY)
 
 var clientText = new ServiceAClient(TARGET.ENVOY);
 var clientBinary = new ServiceAClientBinary(TARGET.ENVOY);
+var clientExtra = new ServiceExtraClient(TARGET.ENVOY);
+var clientExtraBinary = new ServiceExtraClientBinary(TARGET.ENVOY);
 
 var request = new MessagePing();
 request.setTimestamp(11111);
@@ -44,13 +49,17 @@ clientBinary.ping(request, {}, (err, response) => {
 const { Service } = require('./service');
 console.log('Init services with grpcwebtext mode...')
 var service = new Service(
-    clientText,
-    clientBinary,
+    {
+        clientText,
+        clientBinary,
+        clientExtra,
+        clientExtraBinary,
+    },
     {
         MessagePing: MessagePing,
-        MessagePong: MessagePong
+        MessagePong: MessagePong,
+        StreamingMessagePing: StreamingMessagePing,
     }
 )
 
-console.log(2)
 service.load()
--- pkg/api/v2/gen/grpc-web/cmd/service.js
+++ pkg/api/v2/gen/grpc-web/cmd/service.js
@@ -1,10 +1,9 @@
 const app = {};
 
-app.Service = function (service, serviceBin, ctors) {
-    this.serviceText = service
-    this.serviceBin = serviceBin
-    this.service = null
+app.Service = function (service, ctors) {
+    this.service = service
     this.ctors = ctors
+    this.textEncoded = true
 };
 
 app.Service.INTERVAL = 500;
@@ -23,7 +22,7 @@ app.Service.prototype.ping = function (timestamp) {
     unaryRequest.setTimestamp(timestamp)
 
     var metadata = { 'custom-header-1': 'value1' }
-    var call = this.service.ping(unaryRequest, metadata, function (err, response) {
+    var call = (this.textEncoded ? this.service.clientText : this.service.clientBinary).ping(unaryRequest, metadata, function (err, response) {
         if (err) {
             app.Service.addMessage(`Error code: ${err.code} "${err.message}"`, 'danger')
         } else {
@@ -51,12 +50,46 @@ app.Service.prototype.ping = function (timestamp) {
 //     })
 // }
 
+app.Service.prototype.repeatPing = function (timestamp, count) {
+    if (count > app.Service.MAX_STREAM_MESSAGES) {
+        count = app.Service.MAX_STREAM_MESSAGES;
+    }
+    var streamRequest = new this.ctors.StreamingMessagePing();
+    streamRequest.setTimestamp(timestamp);
+    streamRequest.setMessageCount(count);
+    streamRequest.setMessageInterval(app.Service.INTERVAL);
+
+    var metadata = { 'custom-header-1': 'value2' }
+    var stream = (this.textEncoded ? this.service.clientExtra : this.service.clientExtraBinary).streamingPing(
+        streamRequest,
+        metadata,
+    );
+    stream.on('data', function (response) {
+        app.Service.addMessage(`Resp: ${response.getTimestamp()} - ${response.getServicename()}`, 'success')
+    })
+    stream.on('status', function (status) {
+        if (status.metadata) {
+            console.log('Received metadata')
+            console.log(status.metadata)
+        }
+    })
+    stream.on('error', function (err) {
+        app.Service.addMessage(`Error code: ${err.code} "${err.message}"`, 'danger')
+    })
+    stream.on('end', function () {
+        console.log('Stream end signal received')
+    })
+}
+
 app.Service.prototype.send = function (e) {
     var msg = $('#msg').val().trim()
     $('#msg').val('')
     if (!msg) return false
     if (msg.indexOf(' ') > 0) {
-
+        var count = msg.substr(0, msg.indexOf(' '))
+        if (/^\d+$/.test(count)) {
+            this.repeatPing(msg.substr(msg.indexOf(' ') + 1), count)
+        }
     } else if (/^\d+$/.test(msg)) {
         this.ping(msg)
     } else {
@@ -69,11 +102,11 @@ app.Service.prototype.load = function () {
     var self = this
     $(document).ready(function () {
         $('#send').click(function (e) {
-            self.service = self.serviceText
+            self.textEncoded = true
             self.send(e)
         })
         $('#send-binary').click(function (e) {
-            self.service = self.serviceBin
+            self.textEncoded = false
             self.send(e)
         })
         $('#msg').keyup(function (e) {
