ba4cc1eacab7dc16fbf112d3773cbf2c550f8d8e
namnnp <namnnp@vng.com.vn>
2021-05-20 10:18:34 +0700

    update tracing mux

--- pkg/tracing/mux.go
+++ pkg/tracing/mux.go
@@ -1,10 +1,12 @@
 package tracing
 
 import (
+	"context"
 	"net/http"
 
 	"github.com/opentracing-contrib/go-stdlib/nethttp"
 	"github.com/opentracing/opentracing-go"
+	"google.golang.org/grpc/metadata"
 )
 
 type TraceServerMux struct {
@@ -12,28 +14,52 @@ type TraceServerMux struct {
 	tracer opentracing.Tracer
 }
 
-func NewTracerServerMux(tracer opentracing.Tracer) *TraceServerMux {
+func NewTracerServerMux() *TraceServerMux {
 	return &TraceServerMux{
 		mux:    http.NewServeMux(),
-		tracer: tracer,
+		tracer: GlobalTracer(),
 	}
 }
 
-func (t *TraceServerMux) Handle(pattern string, handler http.Handler) {
+func (t *TraceServerMux) SetTracer(tracer opentracing.Tracer) {
+	t.tracer = tracer
+}
+
+func (t *TraceServerMux) Middleware(handler http.Handler) http.Handler {
 	middleware := nethttp.Middleware(
 		t.tracer,
 		handler,
 		nethttp.OperationNameFunc(func(r *http.Request) string {
-			return "HTTP " + r.Method + pattern
+			return "HTTP " + r.Method + r.URL.String()
 		}),
 		// nethttp.MWSpanObserver(func(sp opentracing.Span, r *http.Request) {
 		// 	sp.SetTag("http.uri", r.URL.EscapedPath())
 		// }),
 	)
+	return middleware
+}
+
+func (t *TraceServerMux) Handle(pattern string, handler http.Handler) {
+	middleware := t.Middleware(handler)
 	t.mux.Handle(pattern, middleware)
 }
 
 // Handler: implementation ServeHTTP
 func (t *TraceServerMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	t.mux.ServeHTTP(w, r)
+}
+
+// inject spanCtx metadata into runtime server mux
+func WithMetadata(ctx context.Context, r *http.Request) metadata.MD {
+	span := opentracing.SpanFromContext(ctx)
+	spanCtx := span.Context()
+	carrier := make(map[string]string)
+	if err := span.Tracer().Inject(
+		spanCtx,
+		opentracing.TextMap,
+		opentracing.TextMapCarrier(carrier),
+	); err != nil {
+		return nil
+	}
+	return metadata.New(carrier)
 }
